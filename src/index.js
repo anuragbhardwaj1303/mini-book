2c90d9ef56553c035594b15e0c6d8dec47bfd922
anuragbhardwaj1303 <1914231.cse.coe@cgc.edu.in>
2021-05-24 14:27:33 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+const img =
+  "https://images-eu.ssl-images-amazon.com/images/I/71JZ0neAP1L._AC_UL200_SR200,200_.jpg";
+const title = "The Blue Umbrella";
+const author = "Ruskin Bond";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book
+        img='https://images-eu.ssl-images-amazon.com/images/I/71JZ0neAP1L._AC_UL200_SR200,200_.jpg'
+        title='The Blue Umbrella'
+        author='Ruskin Bond'
+      />
+      <Book
+        img='https://images-eu.ssl-images-amazon.com/images/I/711hfHGE%2BcL._AC_UL200_SR200,200_.jpg'
+        title='Great Stories for Children'
+        author='Ruskin Bond'
+      />
+      <Book
+        img='https://images-eu.ssl-images-amazon.com/images/I/71AdHA%2BqqwL._AC_UL200_SR200,200_.jpg'
+        title='Think and Grow Rich'
+        author='Napolean Hill'
+      />
+      <Book
+        img='https://images-eu.ssl-images-amazon.com/images/I/71JZ0neAP1L._AC_UL200_SR200,200_.jpg'
+        title='As a Man Thinketh'
+        author='James Allen'
+      />
+      <Book
+        img='https://images-eu.ssl-images-amazon.com/images/I/713jIoMO3UL._AC_UL200_SR200,200_.jpg'
+        title='Sapiens A Brief History of Humankind'
+        author='Yuval Noah Harari'
+      />
+      <Book
+        img='https://images-eu.ssl-images-amazon.com/images/I/819HSIKiWdS._AC_UL200_SR200,200_.jpg'
+        title='Pride & Prejudice'
+        author='Jane Austen'
+      />
+    </section>
+  );
+}
+const Book = (props) => {
+  return (
+    <article className='book'>
+      <img src={props.img} alt='' className='bookimg'></img>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <h3 className='title'>{props.title}</h3>
+
+      <p>{props.author}</p>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
